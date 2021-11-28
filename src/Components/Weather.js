732ad4a232e75b955659e2c99a5ec6bcc20097a0
AleksandrukZakhar <zakharaleksandrukdev@gmail.com>
2021-11-28 17:26:26 +0200

    more changes

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -2,28 +2,25 @@ import { coordinates } from "../Functions/GetLocation";
 import useFetchWeather from "../Functions/useFetchWeather";
 import WeatherCard from "./WeatherCard";
 import Loading from "./Loading";
+import Alert from "./Alert";
 
 const Weather = () => {
-  if (!coordinates) {
-    return null;
-  }
-
   const [data] = useFetchWeather(coordinates.lat, coordinates.lon);
 
-  if (data) {
-    return (
-      <div className="flex flex-row flex-none space-x-10">
-        <WeatherCard />
-        <WeatherCard />
-        <WeatherCard />
-        <WeatherCard />
-        <WeatherCard />
-        <WeatherCard />
-        <WeatherCard />
-      </div>
-    );
-  } else {
+  if (!coordinates) {
     return <Loading />;
+  } else {
+    if (data) {
+      return (
+        <div className="flex flex-row flex-none space-x-10">
+          <WeatherCard />
+        </div>
+      );
+    } else {
+      return (
+        <Alert text="Не можемо отримати данні про погоду спробуйте ще раз через декілька хвилин" />
+      );
+    }
   }
 };
 
