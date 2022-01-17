5ca40a15b2dd7e02b5892a4b8ab4c0f98259d217
AleksandrukZakhar <zakharaleksandrukdev@gmail.com>
2022-01-17 08:50:27 +0200

    FetchWeatherChange

--- src/Components/App.js
+++ src/Components/App.js
@@ -14,13 +14,16 @@ const App = () => {
 
   const FetchWeather = async () => {
     const key = "fb36ff8457a47f22e45bbe7a3777961e";
-    let response = null;
 
-    if (coordinates.lat & (coordinates.lon != null)) {
-      let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${key}&units=metric&lang=ua`;
+    if (coordinates.lat && coordinates.lon != null) {
+      const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${key}&units=metric&lang=ua`;
 
       const res = await fetch(url);
-      response = await res.json();
+      const response = await res.json();
+
+      if (response) {
+        setData(response.daily);
+      }
     } else {
       setTimeout(() => {
         FetchWeather();
@@ -30,12 +33,12 @@ const App = () => {
 
   FetchWeather();
   return (
-    <div>
+    <>
       <Header />
       <Text title="Погода UA" subtitle="Погода з усього світу." />
       <Location setCoordinates={setCoordinates} />
-      <Weather />
-    </div>
+      {data ? <Weather /> : null}
+    </>
   );
 };
 
--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,9 +1,11 @@
 import WeatherCard from "./WeatherCard";
+import WeatherBox from "./WeatherBox";
 
 const Weather = () => {
   return (
     <div className="flex flex-row flex-none space-x-10">
       <WeatherCard />
+      <WeatherBox />
     </div>
   );
 };
